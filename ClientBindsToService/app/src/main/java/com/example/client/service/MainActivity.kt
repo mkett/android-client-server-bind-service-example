3cf9551ffd6f1d085c1aa8d444af724d9d5f8e43
Marina Kettschik <hi@marinakettschik.de>
2024-02-21 15:01:06 +0100

    add custom dangerous permission with permission request example on client side

--- ClientBindsToService/app/src/main/java/com/example/client/service/MainActivity.kt
+++ ClientBindsToService/app/src/main/java/com/example/client/service/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.client.service
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
@@ -11,6 +12,7 @@ import com.example.server.provides.service.IAidlRandomNumber
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_CODE_ASK_PERMISSIONS = 123
     private lateinit var numberService: IAidlRandomNumber
     private var serviceBounded: Boolean = false
 
@@ -30,6 +32,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        val dangPermToRequest: List<String> = checkMissingPermissions()
+        if (dangPermToRequest.isNotEmpty()) {
+            requestPermissions(dangPermToRequest)
+            return
+        }
         bindToService()
     }
 
@@ -65,5 +72,39 @@ class MainActivity : AppCompatActivity() {
     private fun updateUi(value: String) {
         findViewById<TextView>(R.id.tvRandomNumber).text = value
     }
+
+    /**
+     * result after user denied or granted the permission
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_ASK_PERMISSIONS) {
+            //all permissions have been granted
+            if (!grantResults.contains(PackageManager.PERMISSION_DENIED)) {
+                bindToService()
+                return
+            }
+        }
+    }
+
+    /**
+     * verify if user gave already permission
+     */
+    private fun checkMissingPermissions(): List<String> {
+        val permissions: MutableList<String> = ArrayList()
+        if (checkSelfPermission("com.example.permission.RANDOM_NUMBER_PERMISSION") != PackageManager.PERMISSION_GRANTED) {
+            permissions.add("com.example.permission.RANDOM_NUMBER_PERMISSION")
+        }
+        return permissions
+    }
+
+    /**
+     * request needed permission to the user
+     */
+    private fun requestPermissions(permissions: List<String>) {
+        requestPermissions(permissions.toTypedArray<String>(), REQUEST_CODE_ASK_PERMISSIONS)
+    }
+
 
 }
